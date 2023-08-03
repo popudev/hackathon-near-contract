b39f09a052ecb7afc46b55deaf8769221055a3bd
popudev <phuthinh53.it@gmail.com>
2023-08-03 11:20:55 +0700

    feat: internal major

--- src/application/repository/internal_major.rs
+++ src/application/repository/internal_major.rs
@@ -2,6 +2,8 @@ use near_sdk::{borsh::BorshSerialize, collections::UnorderedSet, AccountId};
 
 use crate::models::{
   contract::{ContractStorageKey, SuperSchoolContract},
+  major::MajorId,
+  subject::SubjectId,
   user::UserId,
 };
 
@@ -9,15 +11,36 @@ use super::hash_string;
 
 impl SuperSchoolContract {
   pub(crate) fn internal_add_student_to_major(&mut self, major_id: &String, user_id: &AccountId) {
+    let major_student_id = format!("{}{}", &major_id, &user_id);
     let mut users_set = self.students_per_major.get(&major_id).unwrap_or_else(|| {
       UnorderedSet::new(
-        ContractStorageKey::SubjectsPerMajorInter { major_id_hash: hash_string(major_id) }.try_to_vec().unwrap(),
+        ContractStorageKey::StudentsPerMajorInter {
+          major_student_id_hash: hash_string(&major_student_id),
+        }
+        .try_to_vec()
+        .unwrap(),
       )
     });
 
-    //we insert the token ID into the set
     users_set.insert(user_id);
 
     self.students_per_major.insert(major_id, &users_set);
+  }
+
+  pub(crate) fn internal_add_subject_to_major(
+    &mut self,
+    subject_id: &SubjectId,
+    major_id: &MajorId,
+  ) {
+    let subject_major_id_hash = hash_string(&format!("{}{}", &subject_id, &major_id));
+    let mut set = self.subjects_per_major.get(&major_id).unwrap_or_else(|| {
+      UnorderedSet::new(
+        ContractStorageKey::SubjectsPerMajorInter { subject_major_id_hash }.try_to_vec().unwrap(),
+      )
+    });
+
+    set.insert(subject_id);
+
+    self.subjects_per_major.insert(major_id, &set);
   }
 }
